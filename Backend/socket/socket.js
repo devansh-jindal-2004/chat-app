1f7a9b4734ce7ad629a441cb4cce5d1172d531fa
Devansh <devanshjindal31@gmail.com>
2024-07-09 13:44:57 +0530

    socket

--- Backend/socket/socket.js
+++ Backend/socket/socket.js
@@ -7,7 +7,7 @@ const app = express()
 const server = http.createServer(app);
 const io = new Server(server,{
     cors:{
-        origin:["https://chat-app-bdol.onrender.com/"],
+        origin:["*"],
         methods:["GET", "POST"]
     }
 })
