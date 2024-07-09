63e1b017de13d3ffdec6a5df64f65571f924c2f1
Devansh <devanshjindal31@gmail.com>
2024-07-09 11:23:48 +0530

    deployment stage starts

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
@@ -12,6 +13,8 @@ import { app, server } from "./socket/socket.js";
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 app.use(cookieParser());
 app.use(express.json()); // to parse the incoming data in JSON format in req.body
@@ -25,6 +28,12 @@ app.use(cors({
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/Front-End/dist")))
+
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "Front-End", "dist", "index.html"))
+})
 
 // Creating server
 server.listen(PORT, () => {
--- Backend/utils/generateToken.js
+++ Backend/utils/generateToken.js
@@ -6,7 +6,8 @@ const generateTokenAndSetCookie = (userId, res) => {
     res.cookie("jwt",token, {
         maxAge: 3*24*60*60*1000,
         httpOnly:true, //prevents xxs attacks also known as cross site attacks
-        sameSite: "strict" //crsp attacks
+        sameSite: "strict", //crsp attacks
+        secure: true
     })
 }
 
