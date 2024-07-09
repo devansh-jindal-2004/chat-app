05109e7823bacb27d1fab154ebf71cc0bfde1c7a
Devansh <devanshjindal31@gmail.com>
2024-07-09 13:55:30 +0530

    auto scroll

--- Front-End/src/components/messagebox/Messages.jsx
+++ Front-End/src/components/messagebox/Messages.jsx
@@ -9,48 +9,31 @@ function Messages() {
   useListenMessages();
   const lastMsgRef = useRef();
 
-  useEffect(()=>{
-    lastMsgRef.current?.scrollIntoView({bahavior: "smooth"})
-  },[messages])
+  useEffect(() => {
+    lastMsgRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
   
   return (
     <div className='px-4 flex-1 overflow-auto'>
-      {!loading && messages.length > 0 && messages.map((message) => (
-      <div key={message._id} ref={lastMsgRef}>
-        <Message message={message} />
-      </div>
+      {!loading && messages.length > 0 && messages.map((message, index) => (
+        <div key={message._id} ref={index === messages.length - 1 ? lastMsgRef : null}>
+          <Message message={message} />
+        </div>
       ))}
-      {loading && (<><MessageSkelton /> <MessageSkelton /><MessageSkelton /> <MessageSkelton /><MessageSkelton /></>)}
-      {!loading && messages.length == 0 && (<p className='text-center text-gray-200'>send a message to this conversation</p>)}
+      {loading && (
+        <>
+          <MessageSkelton />
+          <MessageSkelton />
+          <MessageSkelton />
+          <MessageSkelton />
+          <MessageSkelton />
+        </>
+      )}
+      {!loading && messages.length === 0 && (
+        <p className='text-center text-gray-200'>Send a message to this conversation</p>
+      )}
     </div>
   );
 }
 
 export default Messages;
-
-
-// starter code
-
-// import React from 'react'
-// import Message from './Message'
-
-// function Messages() {
-//   return (
-//     <div className='px-4 flex-1 overflow-auto'>
-//         <Message />
-//         <Message />
-//         <Message />
-//         <Message />
-//         <Message />
-//         <Message />
-//         <Message />
-//         <Message />
-//         <Message />
-//         <Message />
-//         <Message />
-//         <Message />
-//     </div>
-//   )
-// }
-
-// export default Messages
